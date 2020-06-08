d291c0ee29e2ea386b036cc59c8eb6235d5db0d5
Leonhard <leonhard.heindl@gmail.com>
2020-06-08 15:24:44 +0200

    Commit - Test 5

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -1,8 +1,8 @@
 package mars.rover;
 public class MarsRover {
 
-    private int fieldx;
-    private int fieldy;
+    private static int fieldx;
+    private static int fieldy;
 
     private int posX;
     private int posY;
@@ -30,6 +30,7 @@ public class MarsRover {
     }
 
     public static String move(int x, int y, char direction, String instructions) {
+        char ursprungsDirection = direction;
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
             switch(instruction){
@@ -59,13 +60,17 @@ public class MarsRover {
                 case 'M':
                     switch(direction){
                         case 'N':
-                            return move(x, y+1, 'W', instructions.substring(1));
+                            if(y+1 > fieldy){return move(x, y, 'W', instructions.substring(1));}
+                            else{return move(x, y+1, 'W', instructions.substring(1));}
                         case 'W':
-                            return move(x, y-1, 'S', instructions.substring(1));
+                            if(y-1 < 0){ return move(x, y, 'S', instructions.substring(1));}
+                            else{return move(x, y-1, 'S', instructions.substring(1));}
                         case 'S':
-                            return move(x-1, y, 'E', instructions.substring(1));
+                            if(x-1 < 0){ return move(x, y, 'S', instructions.substring(1));}
+                            else{return move(x, x-1, 'S', instructions.substring(1));}
                         case 'E':
-                            return move(x+1, y, 'N', instructions.substring(1));
+                            if(x+1 > fieldx){return move(x, y, 'W', instructions.substring(1));}
+                            else{return move(x, x+1, 'W', instructions.substring(1));}
                     }
             }
 
