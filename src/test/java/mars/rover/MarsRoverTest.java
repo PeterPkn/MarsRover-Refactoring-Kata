1b4d5ce305b1e27cbb3aaed9d0f0485a3c96ef3d
ClockTester45 <7138@htl.rennweg.at>
2020-06-08 15:27:17 +0200

    Unit Tests 7-10

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -47,4 +47,21 @@ class MarsRoverTest {
         String newPosition = MarsRover.move(0, 0, 'S', "MMML");
         assertEquals("0 -3 E", newPosition);
     }
+    @Test
+    public void acceptance_test_8() {
+        String newPosition = MarsRover.move(0, 0, 'S', "MMML");
+        assertEquals("0 -3 E", newPosition);
+    }
+
+    @Test
+    public void acceptance_test_9() {
+        String newPosition = MarsRover.move(0, 0, 'S', "MMML");
+        assertEquals("0 -3 E", newPosition);
+    }
+
+    @Test
+    public void acceptance_test_10() {
+        String newPosition = MarsRover.move(0, 0, 'S', "MMML");
+        assertEquals("0 -3 E", newPosition);
+    }
 }
