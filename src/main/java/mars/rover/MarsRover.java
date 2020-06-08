f1a0399ddfa43e7d3f659815e9ce0a4ac8227b43
PeterPkn <petsch.pb@gmail.com>
2020-06-08 13:42:27 +0200

    Revert "Kommentare hinzugefügt + instructions.length() entfernt"
    This reverts commit 37da9f472f9e93e18f3f37f22a8c20e8f024664d.

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -21,38 +21,35 @@ public class MarsRover {
     public static String move(int x, int y, char direction, String instructions) {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
-            //dreht sich 90° nach Links
             if (instruction == 'L') {
                 if (direction == 'N') {
-                    return move(x, y, 'W', instructions.substring(1));
+                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x, y, 'S', instructions.substring(1));
+                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y, 'E', instructions.substring(1));
+                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x, y, 'N', instructions.substring(1));
+                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
                 }
-                //dreht sich 90° nach Rechts
             } else if (instruction == 'R') {
                 if (direction == 'N') {
-                    return move(x, y, 'E', instructions.substring(1));
+                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x, y, 'N', instructions.substring(1));
+                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y, 'W', instructions.substring(1));
+                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x, y, 'S', instructions.substring(1));
+                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
                 }
-                //geht um 1 Feld nach vorne
             } else if (instruction == 'M') {
                 if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1));
+                    return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1));
+                    return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1));
+                    return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1));
+                    return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
                 }
             }
         }
