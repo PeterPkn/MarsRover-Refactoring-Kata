8bc6f18e92d0d40e186c8242922ca973b7f820c2
ClockTester45 <7138@htl.rennweg.at>
2020-06-08 16:47:54 +0200

    Deleting of usless functions

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -4,29 +4,9 @@ public class MarsRover {
     private static int fieldx;
     private static int fieldy;
 
-    private int posX;
-    private int posY;
-    private char direction;
-
     public MarsRover(int fieldx, int fieldy) {
         this.fieldx = fieldx;
         this.fieldy = fieldy;
-    }
-
-    public void setPosX(int posX) {
-        if(posX>0 || posX<this.fieldx) {
-            this.posX = posX;
-        }
-    }
-
-    public void setPosY(int posY) {
-        if(posY>0 || posY<this.fieldy) {
-            this.posY = posY;
-        }
-    }
-
-    public void setDirection(char direction) {
-        this.direction = direction;
     }
 
     public static String move(int x, int y, char direction, String instructions) {
@@ -60,17 +40,17 @@ public class MarsRover {
                 case 'M':
                     switch(direction){
                         case 'N':
-                            if(y+1 > fieldy){return move(x, y, 'N', instructions.substring(1));}
-                            else{return move(x, y+1, 'N', instructions.substring(1));}
+                            if(y+1 > fieldy){return move(x, y, 'W', instructions.substring(1));}
+                            else{return move(x, y+1, 'W', instructions.substring(1));}
                         case 'W':
                             if(y-1 < 0){ return move(x, y, 'S', instructions.substring(1));}
                             else{return move(x, y-1, 'S', instructions.substring(1));}
                         case 'S':
-                            if(x-1 < 0){ return move(x, y, 'W', instructions.substring(1));}
-                            else{return move(x, x-1, 'W', instructions.substring(1));}
+                            if(x-1 < 0){ return move(x, y, 'S', instructions.substring(1));}
+                            else{return move(x, x-1, 'S', instructions.substring(1));}
                         case 'E':
-                            if(x+1 > fieldx){return move(x, y,'E', instructions.substring(1));}
-                            else{return move(x, x+1, 'E', instructions.substring(1));}
+                            if(x+1 > fieldx){return move(x, y, 'W', instructions.substring(1));}
+                            else{return move(x, x+1, 'W', instructions.substring(1));}
                     }
             }
 
