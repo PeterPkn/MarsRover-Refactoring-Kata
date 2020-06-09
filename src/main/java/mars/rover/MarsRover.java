22e3bedaf2fc852811c11bace26e5c11c2051b06
Stefan <stefan.huendler2@gmail.com>
2020-06-10 00:52:24 +0200

    Unit Tests 1-5

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -1,4 +1,5 @@
 package mars.rover;
+
 public class MarsRover {
 
     private static int fieldx;
@@ -12,9 +13,9 @@ public class MarsRover {
     public static String move(int x, int y, char direction, String instructions) {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
-            switch(instruction){
+            switch (instruction) {
                 case 'L':
-                    switch(direction){
+                    switch (direction) {
                         case 'N':
                             return move(x, y, 'W', instructions.substring(1));
                         case 'W':
@@ -26,7 +27,7 @@ public class MarsRover {
                     }
                     break;
                 case 'R':
-                    switch(direction){
+                    switch (direction) {
                         case 'N':
                             return move(x, y, 'E', instructions.substring(1));
                         case 'W':
@@ -37,32 +38,30 @@ public class MarsRover {
                             return move(x, y, 'S', instructions.substring(1));
                     }
                 case 'M':
-                    switch(direction){
+                    switch (direction) {
                         case 'N':
-                            if(y+1 > fieldy){
+                            if (y + 1 > fieldy) {
                                 return move(x, y, 'N', instructions.substring(1));
-                            } else{
-                                return move(x, y+1, 'N', instructions.substring(1));
+                            } else {
+                                return move(x, y + 1, 'N', instructions.substring(1));
                             }
                         case 'W':
-                            if(x-1 < 0){
+                            if (x - 1 < 0) {
                                 return move(x, y, 'W', instructions.substring(1));
-                            } else{
-                                return move(x-1, y, 'W', instructions.substring(1));
+                            } else {
+                                return move(x - 1, y, 'W', instructions.substring(1));
                             }
                         case 'S':
-                            if(y-1 < 0){
+                            if (y - 1 < 0) {
                                 return move(x, y, 'S', instructions.substring(1));
-                            }
-                            else{
-                                return move(x, y-1, 'S', instructions.substring(1));
+                            } else {
+                                return move(x, y - 1, 'S', instructions.substring(1));
                             }
                         case 'E':
-                            if(x+1 > fieldx){
-                                return move(x, y,'E', instructions.substring(1));
-                            }
-                            else{
-                                return move(x+1, y, 'E', instructions.substring(1));
+                            if (x + 1 > fieldx) {
+                                return move(x, y, 'E', instructions.substring(1));
+                            } else {
+                                return move(x + 1, y, 'E', instructions.substring(1));
                             }
                     }
             }
--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -7,65 +7,71 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MarsRoverTest {
 
     @Test
-    public void acceptance_test_1() {
-        String newPosition = MarsRover.move(1, 2, 'N', "LMLMLMLMM");
-        assertEquals("1 3 N", newPosition);
+    public void acceptance_test_1DirectionTestNorth() {
+        MarsRover test = new MarsRover(5, 5);
+        String newPosition = test.move(0, 0, 'N', "MMMRMLLM");
+        assertEquals("0 3 W", newPosition);
     }
 
     @Test
-    public void acceptance_test_2() {
-        String newPosition = MarsRover.move(3, 3, 'E', "MMRMMRMRRM");
-        assertEquals("5 1 E", newPosition);
+    public void acceptance_test_2DirectionTestEast() {
+        MarsRover test = new MarsRover(10, 10);
+        String newPosition = test.move(3, 1, 'O', "MMMRMLLM");
+        assertEquals("3 1 O", newPosition);
     }
 
     @Test
-    public void acceptance_test_3() {
-        String newPosition = MarsRover.move(0, 0, 'S', "MMML");
-        assertEquals("0 -3 E", newPosition);
+    public void acceptance_test_3DirectionSouth() {
+        MarsRover test = new MarsRover(3, 1);
+        String newPosition = test.move(0, 0, 'S', "MRRMLRMM");
+        assertEquals("0 1 N", newPosition);
     }
 
     @Test
-    public void acceptance_test_4() {
-        String neuePosition = MarsRover.move(5, 3, 'S', "MMRM");
-        assertEquals("6 3 N", neuePosition);
+    public void acceptance_test_4DirectionWest() {
+        MarsRover test = new MarsRover(7, 7);
+        String neuePosition = MarsRover.move(5, 3, 'S', "RRRLLLMMMLM");
+        assertEquals("6 0 E", neuePosition);
     }
 
     @Test
-    public void acceptance_test_5() {
-        String newPosition = MarsRover.move(0, 0, 'S', "MMML");
-        assertEquals("0 -3 E", newPosition);
+    public void acceptance_test_5NullTravel() {
+        MarsRover test = new MarsRover(0, 0);
+        String newPosition = test.move(0, 0, 'S', "");
+        assertEquals("0 0 S", newPosition);
     }
 
     @Test
     public void acceptance_test_6BorderNorth() {
-        MarsRover test = new MarsRover(5,5);
+        MarsRover test = new MarsRover(5, 5);
         String newPosition = test.move(3, 3, 'S', "LLMMM");
         assertEquals("3 5 N", newPosition);
     }
 
     @Test
     public void acceptance_test_7BorderEast() {
-        MarsRover test = new MarsRover(2,2);
+        MarsRover test = new MarsRover(2, 2);
         String newPosition = test.move(0, 0, 'S', "MLM");
         assertEquals("1 0 E", newPosition);
     }
+
     @Test
     public void acceptance_test_8BorderSouth() {
-        MarsRover test = new MarsRover(5,5);
+        MarsRover test = new MarsRover(5, 5);
         String newPosition = test.move(3, 3, 'S', "MLLRRMMM");
         assertEquals("3 0 S", newPosition);
     }
 
     @Test
     public void acceptance_test_9BorderWest() {
-        MarsRover test = new MarsRover(2,3);
+        MarsRover test = new MarsRover(2, 3);
         String newPosition = test.move(1, 1, 'S', "MRMM");
         assertEquals("0 0 W", newPosition);
     }
 
     @Test
     public void acceptance_test_10LongTravel() {
-        MarsRover test = new MarsRover(5,5);
+        MarsRover test = new MarsRover(5, 5);
         String newPosition = test.move(3, 3, 'W', "MLMRMLMLMRMLMLMRMLMLM");
         assertEquals("3 2 W", newPosition);
     }
